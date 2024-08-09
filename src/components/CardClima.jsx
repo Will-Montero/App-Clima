d297d74ed04cd1ef938cacb359f789ec9abef108
Will-Montero <monteromora99@gmail.com>
2024-08-08 22:14:38 -0500

    logica en hijo

--- src/components/CardClima.jsx
+++ src/components/CardClima.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react"
 
 export const CardClima = () => {
-  return (
+
+    const [ciudad, setCiudad] = useState('')
+    const [dataClima, setDataClima] = useState([])
+
+    const URL = 'https://api.openweathermap.org/data/2.5/weather'
+    const API_KEY = '63d11d676a975cda1e0594da8f384257'
+    const difKelvin = 273.15
+
+    const fetchClima = async () => {
+        try {
+            const response = await fetch(`${URL}?q=${ciudad}&appid=${API_KEY}`)
+            const data = await response.json()
+            setDataClima(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleCambioClima = (e) => {
+        setCiudad(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(ciudad.length > 0)fetchClima()
+    }
+
+    return (
   <>
   
   </>
